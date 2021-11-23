0967e7f8f53ab1b1cf2d1636d2a2b8d51628e1b4
dec9k <43190531+dec9k@users.noreply.github.com>
2021-11-22 23:16:00 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const { createServer } = require('http')
+
+const server = createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(JSON.stringify({ a: 1, b: 2, c: 3}));
+    return response.end();
+});
+
+server.listen(8080);
